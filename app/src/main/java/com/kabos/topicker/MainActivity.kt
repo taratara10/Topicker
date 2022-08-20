27f72ffa7a34a009f954786814fcd64f1a6878e7
taratara10 <shuhei.knhs@gmail.com>
2022-08-20 17:04:12 +0900

    pagerをとりあえず作る

--- app/src/main/java/com/kabos/topicker/MainActivity.kt
+++ app/src/main/java/com/kabos/topicker/MainActivity.kt
@@ -9,10 +9,16 @@ import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.pager.ExperimentalPagerApi
+import com.google.accompanist.pager.rememberPagerState
 import com.kabos.topicker.ui.theme.TopickerTheme
+import com.kabos.topicker.ui.topic.TopicPager
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -22,7 +28,15 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    val pagerState = rememberPagerState()
+                    TopicPager(
+                        pagerState = pagerState,
+                        pageCount = 10,
+                        modifier = Modifier,
+                        pagerColors = listOf(Color.Blue, Color.Cyan)
+                    ) { page ->
+                        Text(text = "page is $page")
+                    }
                 }
             }
         }
