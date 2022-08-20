de677dfbb50d5ba59ff7b53f6481f93ad36ef058
taratara10 <shuhei.knhs@gmail.com>
2022-08-20 20:42:38 +0900

    topAppbarを追加

---
+++ app/src/main/java/com/kabos/topicker/ui/topic/PagerTopAppBar.kt
@@ -0,0 +1,54 @@
+package com.kabos.topicker.ui.topic
+
+import android.content.res.Configuration
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.kabos.topicker.ui.theme.TopickerTheme
+
+
+@Composable
+fun PagerTopAppBar(
+    modifier: Modifier = Modifier,
+    contentColor: Color = Color.White,
+) {
+    TopAppBar(
+        title = { },
+        backgroundColor = Color.Transparent,
+        elevation = 0.dp,
+        navigationIcon = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.KeyboardArrowLeft,
+                    contentDescription = "Left Icon",
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { /*TODO*/ }) {
+                Icon(
+                    imageVector = Icons.Default.MoreVert,
+                    contentDescription = "Menu icon",
+                )
+            }
+        },
+        contentColor = contentColor,
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+@Composable
+fun PreviewPagerTopAppBar() {
+    TopickerTheme {
+        PagerTopAppBar(contentColor = Color.Black)
+    }
+}
