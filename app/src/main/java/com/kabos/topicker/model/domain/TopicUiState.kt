d73b6d5082ecf51725db6bbb4a1926abfa586b15
taratara10 <shuhei.knhs@gmail.com>
2022-08-21 17:19:04 +0900

    会話状況のstateを追加してみる

--- app/src/main/java/com/kabos/topicker/model/domain/TopicUiState.kt
+++ app/src/main/java/com/kabos/topicker/model/domain/TopicUiState.kt
@@ -5,6 +5,11 @@ import androidx.compose.ui.graphics.Color
 data class TopicUiState (
     val title: String,
     val color: Color,
-
-
+    val conversationState: ConversationState
     )
+
+sealed class ConversationState {
+    object UnSelected: ConversationState()
+    object Complete: ConversationState()
+    object Skip: ConversationState()
+}
