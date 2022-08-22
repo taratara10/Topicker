5eb5585232b33189366dfe97b9823b4fe10d6c8c
taratara10 <shuhei.knhs@gmail.com>
2022-08-22 23:18:04 +0900

    repositoryとViewModelを繋いでみる

--- app/src/main/java/com/kabos/topicker/model/domain/TopicUiState.kt
+++ app/src/main/java/com/kabos/topicker/model/domain/TopicUiState.kt
@@ -1,15 +1,29 @@
 package com.kabos.topicker.model.domain
 
 import androidx.compose.ui.graphics.Color
+import com.kabos.topicker.model.data.Topic
 
-data class TopicUiState (
+// TODO pageStateでは？？？
+data class TopicUiState(
     val title: String,
     val color: Color,
     val conversationState: ConversationState
-    )
+) {
+   companion object {
+       fun of(topic: Topic): TopicUiState =
+           TopicUiState(
+               title = topic.title,
+               color = colors.random(),
+               conversationState = ConversationState.UnSelected,
+           )
+   }
+}
 
 sealed class ConversationState {
-    object UnSelected: ConversationState()
-    object Complete: ConversationState()
-    object Skip: ConversationState()
+    object UnSelected : ConversationState()
+    object Complete : ConversationState()
+    object Skip : ConversationState()
 }
+
+// TODO 仮置き
+val colors = listOf(Color.Cyan, Color.Green, Color.Yellow, Color.Blue, Color.DarkGray, Color.Magenta)
