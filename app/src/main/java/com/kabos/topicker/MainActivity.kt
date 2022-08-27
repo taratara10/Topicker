9d463e3baa6c2b3d8daaa93a4b8c3bec6929e68c
taratara10 <64339483+taratara10@users.noreply.github.com>
2022-08-27 11:25:26 +0900

    ステータスバーの透過 (#29)
    * ステータスバーを透過
    
    * コメントを追加

--- app/src/main/java/com/kabos/topicker/MainActivity.kt
+++ app/src/main/java/com/kabos/topicker/MainActivity.kt
@@ -1,31 +1,19 @@
 package com.kabos.topicker
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.lifecycleScope
+import androidx.core.view.WindowCompat
 import com.google.accompanist.pager.ExperimentalPagerApi
 import com.google.accompanist.pager.rememberPagerState
-import com.kabos.topicker.model.domain.ConversationState
-import com.kabos.topicker.model.domain.TopicUiState
 import com.kabos.topicker.ui.theme.TopickerTheme
 import com.kabos.topicker.ui.topic.TopicPagerScreen
-import com.kabos.topicker.ui.topic.TopicViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
@@ -33,8 +21,8 @@ class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalPagerApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTransparentStatusBar()
         setContent {
-
             TopickerTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
@@ -46,6 +34,12 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+    private fun setTransparentStatusBar() {
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        window.navigationBarColor = Color.TRANSPARENT
     }
 }
 
