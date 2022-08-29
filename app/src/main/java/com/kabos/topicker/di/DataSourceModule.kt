02399e406c592e233ae3ee3f284415cc86b60c92
taratara10 <shuhei.knhs@gmail.com>
2022-08-29 22:56:12 +0900

    firebastoreをDI

---
+++ app/src/main/java/com/kabos/topicker/di/DataSourceModule.kt
@@ -0,0 +1,25 @@
+package com.kabos.topicker.di
+
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.firestoreSettings
+import com.google.firebase.ktx.Firebase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@InstallIn(SingletonComponent::class)
+@Module
+object DataSourceModule {
+    @Singleton
+    @Provides
+    fun provideFirestore(): FirebaseFirestore =
+        Firebase.firestore.apply {
+            firestoreSettings = firestoreSettings {
+                //オフラインキャッシュの有効化
+                isPersistenceEnabled = true
+            }
+        }
+}
