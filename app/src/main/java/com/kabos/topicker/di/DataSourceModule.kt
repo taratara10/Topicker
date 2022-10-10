cbfd737c062b2abca97bada635c3ee58382a873e
taratara10 <shuhei.knhs@gmail.com>
2022-10-10 22:07:28 +0900

    diしてみた

--- app/src/main/java/com/kabos/topicker/di/DataSourceModule.kt
+++ app/src/main/java/com/kabos/topicker/di/DataSourceModule.kt
@@ -1,13 +1,25 @@
 package com.kabos.topicker.di
 
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.core.handlers.ReplaceFileCorruptionHandler
+import androidx.datastore.preferences.core.PreferenceDataStoreFactory
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.emptyPreferences
+import androidx.datastore.preferences.preferencesDataStoreFile
 import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.firestoreSettings
 import com.google.firebase.ktx.Firebase
+import com.kabos.datastore.UserDataStore
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
 import javax.inject.Singleton
 
 @InstallIn(SingletonComponent::class)
@@ -22,4 +34,21 @@ object DataSourceModule {
                 isPersistenceEnabled = true
             }
         }
+
+    @Singleton
+    @Provides
+    fun providePreferenceDataStore(
+        @ApplicationContext appContext: Context
+    ): DataStore<Preferences> =
+        PreferenceDataStoreFactory.create(
+            corruptionHandler = ReplaceFileCorruptionHandler { emptyPreferences() },
+            scope = CoroutineScope(Dispatchers.IO + SupervisorJob()),
+            produceFile =  { appContext.preferencesDataStoreFile("setting") }
+        )
+
+    @Singleton
+    @Provides
+    fun provideUserDataStore(
+        userPreference: DataStore<Preferences>
+    ): UserDataStore = UserDataStore(userPreference)
 }
