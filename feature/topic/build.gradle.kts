e552c517dcbf29fcc1d02e2717c94d93a14fc467
taratara10 <shuhei.knhs@gmail.com>
2022-10-22 17:42:06 +0900

    とりあえずfeature切った

---
+++ feature/topic/build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("android")
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.kabos.topicker.feature.topic"
+    compileSdk = 33
+}
+
+dependencies {
+    implementation(project(":core:model"))
+    implementation(project(":core:domain"))
+
+
+    implementation(libs.bundles.androidx.base)
+    implementation(libs.bundles.androidx.compose)
+    implementation(libs.compose.lottie)
+    implementation(libs.timber)
+}
