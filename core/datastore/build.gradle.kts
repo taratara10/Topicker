0a7fbcd4397dc179a908881928718342ac7d6236
taratara10 <shuhei.knhs@gmail.com>
2022-10-22 17:05:00 +0900

    不要な依存を削除

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -6,29 +6,10 @@ plugins {
 android {
     namespace = "com.kabos.datastore"
     compileSdk = 33
-
-    defaultConfig {
-        minSdk = 26
-        targetSdk = 33
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-    }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
-
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.appcompat:appcompat:1.5.1")
     testImplementation(libs.test.junit)
-    androidTestImplementation(libs.bundles.test.android)
 
     implementation(libs.datastore)
 }
