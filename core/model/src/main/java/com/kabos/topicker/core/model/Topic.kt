d840e064a2ce9c6a0475c73c254b759d6932224e
taratara10 <shuhei.knhs@gmail.com>
2022-10-23 18:58:41 +0900

    topicUiStataの中身をtopic -> OwnTopicに変更

--- core/model/src/main/java/com/kabos/topicker/core/model/Topic.kt
+++ core/model/src/main/java/com/kabos/topicker/core/model/Topic.kt
@@ -4,14 +4,7 @@ data class Topic(
     val id: Int,
     val title: String,
     val isFavorite: Boolean,
-) {
-    fun updateFavorite(isFavorite: Boolean): Topic =
-        Topic(
-            id = id,
-            title = title,
-            isFavorite = isFavorite,
-        )
-}
+)
 
 val previewTopics = listOf(
     Topic(1, "sample1", false),
--- feature/topic/src/main/java/com/kabos/topicker/feature/topic/collection/TopicUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.kabos.topicker.feature.topic.collection
-
-import com.kabos.topicker.core.model.OwnTopic
-
-data class TopicUiState(
-    val screenTopics: List<OwnTopic>
-)
-
