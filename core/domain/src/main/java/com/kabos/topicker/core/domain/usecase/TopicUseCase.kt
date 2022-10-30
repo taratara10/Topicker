a1587e16abd8d58d9cf9b1d2cd165e917c4c7b57
taratara10 <shuhei.knhs@gmail.com>
2022-10-30 20:35:02 +0900

    ややバグだけど一応うごく

--- core/domain/src/main/java/com/kabos/topicker/core/domain/usecase/TopicUseCase.kt
+++ core/domain/src/main/java/com/kabos/topicker/core/domain/usecase/TopicUseCase.kt
@@ -4,6 +4,7 @@ import com.kabos.topicker.core.domain.repository.TopicRepository
 import com.kabos.topicker.core.model.OwnTopic
 import com.kabos.topicker.core.model.Topic
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.stateIn
@@ -13,18 +14,6 @@ class TopicUseCase(
     private val topicRepository: TopicRepository,
     private val ioScope: CoroutineScope
 ) {
-    companion object {
-        /** topicのid範囲 ver1.0では360個 */
-        val RANGE = 0..360
-    }
-
-    private val leadTopic = listOf(
-        OwnTopic(10000, " Let's go! \uD83D\uDC49", false),
-        OwnTopic(100001, "sample2", false),
-    )
-
-    private val _screenTopics: MutableStateFlow<List<OwnTopic>> = MutableStateFlow(leadTopic)
-    val screenTopics: StateFlow<List<OwnTopic>> = _screenTopics
 
     private val _ownTopics: MutableStateFlow<List<OwnTopic>> = MutableStateFlow(listOf())
     val ownTopics: StateFlow<List<OwnTopic>> = _ownTopics
@@ -33,40 +22,26 @@ class TopicUseCase(
         ioScope.launch {
             topicRepository.getOwnTopics().stateIn(ioScope).collect { ownTopics ->
                 _ownTopics.value = ownTopics
-                _screenTopics.emit(reflectStatus(screenTopics.value, ownTopics))
             }
         }
     }
 
-    /**
-     * [ownTopics]の更新内容を[screenTopics]に反映したリストを作る
-     * */
-    private fun reflectStatus(screenTopics: List<OwnTopic>, allOwnTopics: List<OwnTopic>): List<OwnTopic> {
-        return screenTopics.map { screenTopic ->
-            allOwnTopics.find { it.topicId == screenTopic.topicId } ?: screenTopic
-        }
+    suspend fun getOwnTopics(): Flow<List<OwnTopic>> {
+        return topicRepository.getOwnTopics()
     }
 
     /**
-     * 画面にランダムなtopicを追加する
-     * ownTopicに登録されていない場合、収集済みとしてownTopicに追加する
+     * TODO
      * */
-    suspend fun addScreenTopics() {
+    suspend fun addOwnTopicIfNeeded(topicId: Int) {
+        // TODO ErrorHandling
         val topic =
-            topicRepository.getTopicById(RANGE.random()) ?: throw Exception("Cannot add topics")
+            topicRepository.getTopicById(topicId) ?: throw Exception("Cannot add topics")
 
-        val registeredOwnTopic: OwnTopic? = ownTopics.value.find { it.topicId == topic.id }
-
-        val ownTopic: OwnTopic =
-            if (registeredOwnTopic != null) {
-                registeredOwnTopic
-            } else {
-                val newOwnTopic = topic.toOwnTopic()
-                topicRepository.addOwnTopic(newOwnTopic)
-                newOwnTopic
-            }
-
-        _screenTopics.emit(_screenTopics.value + ownTopic)
+        val shouldRegisterAsOwnTopic = (ownTopics.value.find { it.topicId == topic.id } == null)
+        if (shouldRegisterAsOwnTopic) {
+            topicRepository.addOwnTopic(topic.toOwnTopic())
+        }
     }
 
     suspend fun updateFavoriteState(topicId: Int, isFavorite: Boolean) {
--- feature/topic/src/main/java/com/kabos/topicker/feature/topic/collection/TopicViewModel.kt
+++ feature/topic/src/main/java/com/kabos/topicker/feature/topic/collection/TopicViewModel.kt
@@ -6,39 +6,68 @@ import androidx.lifecycle.viewModelScope
 import com.kabos.topicker.core.domain.usecase.TopicUseCase
 import com.kabos.topicker.core.model.OwnTopic
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @Stable
 @HiltViewModel
 class TopicViewModel @Inject constructor(
-    private val topicUseCase: TopicUseCase
+    private val topicUseCase: TopicUseCase,
 ) : ViewModel() {
+    companion object {
+        /** topicのid範囲 ver1.0では360個 */
+        val RANGE = 0..360
+        val TUTORIAL = listOf(
+            OwnTopic(10000, " Let's go! \uD83D\uDC49", false),
+            OwnTopic(100001, "sample2", false),
+        )
+    }
 
-    private val _topicUiState: MutableStateFlow<TopicUiState> = MutableStateFlow(TopicUiState.Loading)
+    private var _topicUiState: MutableStateFlow<TopicUiState> =
+        MutableStateFlow(TopicUiState.Loading)
     val topicUiState: StateFlow<TopicUiState> = _topicUiState
+
+    /** topicScreenに表示しているtopic 1ページ目のtutorialは除く */
+    private val screenTopicIds: MutableStateFlow<List<Int>> = MutableStateFlow(listOf())
 
     init {
         viewModelScope.launch {
-            topicUseCase.screenTopics.collect { topics ->
-                _topicUiState.emit(TopicUiState.Success(topics))
+
+            topicUseCase.getOwnTopics().combine(screenTopicIds) { ownTopics, screenTopicIds ->
+                TopicUiState.Success(
+                    TUTORIAL + ownTopics.filter { screenTopicIds.contains(it.topicId) }
+                )
+                // todo errorは？
+            }.stateIn(
+                scope = viewModelScope,
+                started = SharingStarted.WhileSubscribed(5_000),
+                initialValue = TopicUiState.Loading,
+            ).collect {
+                _topicUiState.value = it
             }
         }
+        addScreenTopicId()
     }
 
     fun addTopic() = viewModelScope.launch {
-        topicUseCase.addScreenTopics()
+        val addTopicId = addScreenTopicId()
+        topicUseCase.addOwnTopicIfNeeded(addTopicId)
     }
 
-    fun updateFavoriteState(id: Int, isFavorite: Boolean) = viewModelScope.launch{
+    fun updateFavoriteState(id: Int, isFavorite: Boolean) = viewModelScope.launch {
         topicUseCase.updateFavoriteState(id, isFavorite)
+    }
+
+    private fun addScreenTopicId(): Int {
+        val randomId = RANGE.random()
+        screenTopicIds.value += randomId
+        return randomId
     }
 }
 
 sealed interface TopicUiState {
-    data class Success(val screenTopics: List<OwnTopic>): TopicUiState
-    object Loading: TopicUiState
-    object Error: TopicUiState
+    data class Success(val screenTopics: List<OwnTopic>) : TopicUiState
+    object Loading : TopicUiState
+    object Error : TopicUiState
 }
