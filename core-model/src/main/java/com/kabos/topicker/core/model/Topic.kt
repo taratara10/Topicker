61370831d85a6be7d97ee19d4866498e9d525d88
taratara10 <shuhei.knhs@gmail.com>
2022-11-05 11:48:08 +0900

    isRegister追加

--- core-model/src/main/java/com/kabos/topicker/core/model/Topic.kt
+++ core-model/src/main/java/com/kabos/topicker/core/model/Topic.kt
@@ -3,18 +3,17 @@ package com.kabos.topicker.core.model
 data class Topic(
     val id: Int,
     val title: String,
-    val isFavorite: Boolean,
 )
 
 val previewTopics = listOf(
-    Topic(1, "sample1", false),
-    Topic(2, "sample2", false),
-    Topic(3, "sample3", false),
-    Topic(4, "sample4", false),
-    Topic(5, "sample5", false),
-    Topic(6, "sample6", false),
-    Topic(7, "sample7", false),
-    Topic(8, "sample8", false),
-    Topic(9, "sample9", false),
-    Topic(10, "sample10", false),
+    Topic(1, "sample1"),
+    Topic(2, "sample2"),
+    Topic(3, "sample3"),
+    Topic(4, "sample4"),
+    Topic(5, "sample5"),
+    Topic(6, "sample6"),
+    Topic(7, "sample7"),
+    Topic(8, "sample8"),
+    Topic(9, "sample9"),
+    Topic(10, "sample10"),
 )
